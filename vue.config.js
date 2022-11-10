2ee30e91d5f835d344780c67bc68bb64f0231434
osama <ocamahere@gmail.com>
2022-11-10 20:19:41 +0000

    Base Template

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  // useEslint: false,
+
+  chainWebpack: (config) => {
+    config.module.rules.delete("eslint");
+    config.module.rules.delete("prettier");
+  },
 });
