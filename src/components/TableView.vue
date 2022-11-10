6134ef86418be70efc54667e19137d19ca048ce6
osama <ocamahere@gmail.com>
2022-11-10 22:41:40 +0000

    Companies view component

---
+++ src/components/TableView.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container mx-auto">
+    <div>
+      <div class="mt-4 overflow-x-auto relative">
+        <table
+          class="w-full text-sm text-left text-gray-500 dark:text-gray-400"
+        >
+          <thead
+            class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-900 dark:text-gray-400"
+          >
+            <tr>
+              <th scope="col" class="py-3 px-6">ID</th>
+              <th scope="col" class="py-3 px-6">Title</th>
+              <th scope="col" class="py-3 px-6">Desc</th>
+              <th scope="col" class="py-3 px-6">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="company in companies"
+              :key="company.id"
+              class="bg-white border-b dark:bg-gray-700 dark:border-gray-900"
+            >
+              <td class="py-4 px-6">{{ company.id }}</td>
+              <td class="py-4 px-6">{{ company.title }}</td>
+              <td class="py-4 px-6">{{ company.description }}</td>
+              <td class="py-4 px-6">EDIT</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    companies: Array,
+  },
+  setup(props) {
+    return {
+      props,
+    };
+  },
+};
+</script>
---
+++ src/views/CompaniesView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <TableView :companies="companies" />
+  </div>
+</template>
+
+<script>
+import TableView from "@/components/TableView.vue";
+import { onMounted, ref } from "vue";
+import axios from "axios";
+
+export default {
+  components: {
+    TableView,
+  },
+
+  setup() {
+    // eslint-disable-next-line
+    let companies = ref([]);
+    const getCompanies = async () => {
+      let res = await axios.get("https://dummyjson.com/products");
+      companies.value = res.data.products;
+      // console.log("CHECK COMPANIES ==> ", res.data.products);
+    };
+    onMounted(() => {
+      getCompanies();
+    });
+    return {
+      companies,
+    };
+  },
+};
+</script>
